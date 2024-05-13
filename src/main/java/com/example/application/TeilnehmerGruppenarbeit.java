9ea325831d7de6530aa5b7fd9ef655c93e67a1a5
LilliK99 <klindtworth.lilli@gmail.com>
2024-05-13 13:56:47 +0200

    Hinzufügen der TeilnehmerGruppenarbeit-Tabelle

---
+++ src/main/java/com/example/application/TeilnehmerGruppenarbeit.java
@@ -0,0 +1,22 @@
+package com.example.application;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "teilnehmer_gruppenarbeit")
+public class TeilnehmerGruppenarbeit {
+    @EmbeddedId
+    private TeilnehmerGruppenarbeitId id;
+
+    public TeilnehmerGruppenarbeitId getId() {
+        return id;
+    }
+
+    public void setId(TeilnehmerGruppenarbeitId id) {
+        this.id = id;
+    }
+
+    //TODO [Reverse Engineering] generate columns from DB
+}
