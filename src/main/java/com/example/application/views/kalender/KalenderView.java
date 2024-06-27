adcdbd027045e1eacbde4975b7da664a27edf04a
Leon <leon.lentz@hsbi.de>
2024-06-27 09:32:05 +0200

    LAST COMMIT

--- src/main/java/com/example/application/views/kalender/KalenderView.java
+++ src/main/java/com/example/application/views/kalender/KalenderView.java
@@ -1,5 +1,4 @@
 package com.example.application.views.kalender;
-
 import com.example.application.models.*;
 import com.example.application.security.AuthenticatedUser;
 import com.example.application.services.*;
@@ -18,6 +17,7 @@ import org.vaadin.stefan.fullcalendar.FullCalendarBuilder;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 // LEON
@@ -46,6 +46,11 @@ public class KalenderView extends VerticalLayout {
     Button naechste = new Button("Nächste");
     // Auswahl der Ansichten
     ComboBox<KalenderAuswahl> kalenderAuswahlComboBox = new ComboBox<>();
+    // H1
+    H1 anzeige = new H1(); // Anzeige des Datum oder KW oder Monat
+    // DateTime Formatter - Optionen sind Aktuelles Datum mit dd.mm.yyyy, MMM für Monat
+    DateTimeFormatter dateFormatterMonat = DateTimeFormatter.ofPattern("MMM").withLocale( Locale.GERMAN );
+    DateTimeFormatter dateFormatterStandard = DateTimeFormatter.ofPattern("dd.MM.yyyy").withLocale( Locale.GERMAN );
 
     /**
      * Konstruktor für die KalenderView Klasse.
@@ -132,6 +137,7 @@ public class KalenderView extends VerticalLayout {
      */
     private void configureKalenderAuswahl() {
         kalenderAuswahlComboBox.setItems(KalenderAuswahl.getAllWerte());
+        kalenderAuswahlComboBox.setValue(KalenderAuswahl.getAllWerte().getFirst());
         kalenderAuswahlComboBox.setItemLabelGenerator(KalenderAuswahl::toString);
         kalenderAuswahlComboBox.addValueChangeListener(event -> validateKalenderAuswahl());
     }
@@ -190,7 +196,6 @@ public class KalenderView extends VerticalLayout {
             long veranstaltungId = Long.parseLong(entry.getConstraint());
             getUI().ifPresent(ui -> ui.navigate("veranstaltung-detail/" + veranstaltungId));
         });
-
         calendar.setSizeFull();
         return calendar;
     }
@@ -210,21 +215,37 @@ public class KalenderView extends VerticalLayout {
                         currentDate = getStartOfMonth(currentDate.plusMonths(1)),
                         currentDate.plusMonths(1)
                 );
+                // Es wird der Monat ausgewählt und zunächst jedes Datum davor aus der anzeige entfernt, wenn keine Auswahl getroffen wurde
+                anzeige.removeAll();
+                anzeige.add(currentDate.format(dateFormatterMonat));
+
             } else if (kalenderAuswahlComboBox.getValue().istMonat()) {
                 fullCalendar.setValidRange(
                         currentDate = getStartOfMonth(currentDate.plusMonths(1)),
                         currentDate.plusMonths(1)
                 );
+                // Es wird der Monat ausgewählt und zunächst jedes Datum davor aus der anzeige entfernt
+                anzeige.removeAll();
+                anzeige.add(currentDate.format(dateFormatterMonat));
+
             } else if (kalenderAuswahlComboBox.getValue().istTag()) {
                 fullCalendar.setValidRange(
                         currentDate = currentDate.plusDays(1),
                         currentDate.plusDays(2)
                 );
+                // Es wird der Monat ausgewählt und zunächst jedes Datum davor aus der anzeige entfernt
+                anzeige.removeAll();
+                anzeige.add(currentDate.format(dateFormatterStandard));
+
             } else if (kalenderAuswahlComboBox.getValue().istWoche()) {
                 fullCalendar.setValidRange(
                         currentDate = getStartOfWeek(currentDate.plusWeeks(1)),
                         currentDate.plusWeeks(1)
                 );
+                // Zunächst wird jedes Datum davor aus der anzeige entfernt
+                // Dann wird die KW berechnet
+                anzeige.removeAll();
+                anzeige.add("KW "+ getKW(currentDate.getDayOfYear()));
             }
         } else {
             if (kalenderAuswahlComboBox.getValue() == null) {
@@ -232,23 +253,35 @@ public class KalenderView extends VerticalLayout {
                         currentDate = getStartOfMonth(currentDate.minusMonths(1)),
                         currentDate.plusMonths(1)
                 );
+                // Es wird der Monat ausgewählt und zunächst jedes Datum davor aus der anzeige entfernt
+                anzeige.removeAll();
+                anzeige.add(currentDate.format(dateFormatterMonat));
             } else if (kalenderAuswahlComboBox.getValue().istMonat()) {
                 fullCalendar.setValidRange(
                         currentDate = getStartOfMonth(currentDate.minusMonths(1)),
                         currentDate.plusMonths(1)
                 );
+                // Es wird der Monat ausgewählt und zunächst jedes Datum davor aus der anzeige entfernt
+                anzeige.removeAll();
+                anzeige.add(currentDate.format(dateFormatterMonat));
             } else if (kalenderAuswahlComboBox.getValue().istTag()) {
                 fullCalendar.setValidRange(
                         currentDate = currentDate.minusDays(1), // Ein Tag wird abgezogen
                         currentDate.plusDays(1) // Eine Tag wird zum vorherigen Datum hinzugefügt
                 );
+                // Es wird das Datum ausgewählt und zunächst jedes Datum davor aus der anzeige entfernt
+                anzeige.removeAll();
+                anzeige.add(currentDate.format(dateFormatterStandard));
             } else if (kalenderAuswahlComboBox.getValue().istWoche()) {
                 fullCalendar.setValidRange(
                         currentDate = getStartOfWeek(currentDate.minusWeeks(1)), // Eine Woche wird abgezogen und sichergestellt, dass es der Montag ist
                         getStartOfWeek(currentDate.plusDays(7)) // Eine Woche wird zur vorherigen Operation hinzugefügt
                 );
+                // Zunächst wird jedes Datum davor aus der anzeige entfernt
+                // Dann wird die KW berechnet
+                anzeige.removeAll();
+                anzeige.add("KW "+ getKW(currentDate.getDayOfYear()));
             }
-
         }
     }
     //
@@ -293,8 +326,10 @@ public class KalenderView extends VerticalLayout {
         if (maybeUser.isPresent()) {
             getUI().ifPresent(ui -> ui.navigate("login"));
         }
+        // Initial wird der Monat ausgewählt
+        anzeige.add(currentDate.format(dateFormatterMonat));
 
-        var toolbar = new HorizontalLayout(username, zuvor, naechste, kalenderAuswahlComboBox);
+        var toolbar = new HorizontalLayout(username, zuvor, anzeige, naechste, kalenderAuswahlComboBox);
         toolbar.setAlignItems(Alignment.CENTER);
         toolbar.setJustifyContentMode(JustifyContentMode.BETWEEN);
         toolbar.setWidthFull();
@@ -312,14 +347,31 @@ public class KalenderView extends VerticalLayout {
         if (kalenderAuswahlComboBox.getValue() == null || kalenderAuswahlComboBox.getValue().istMonat()) { // Die neuen Grenzen bilden nur einen Rahmen, daher kann hier auch
             // mit zwei Monaten gerechnet werden
             fullCalendar.setValidRange(getStartOfMonth(currentDate).minusWeeks(1), getStartOfMonth(currentDate).plusMonths(2));
+
+            // Es wird der Monat ausgewählt und zunächst jedes Datum davor aus der anzeige entfernt
+            anzeige.removeAll();
+            anzeige.add(currentDate.format(dateFormatterMonat));
+
             fullCalendar.changeView(CalendarViewImpl.DAY_GRID_MONTH);
         } else if (kalenderAuswahlComboBox.getValue().istTag()) {
             naechste.click();
             zuvor.click();
+
+            // Es wird das Datum ausgewählt und zunächst jedes Datum davor aus der anzeige entfernt
+            anzeige.removeAll();
+            anzeige.add(currentDate.format(dateFormatterStandard));
+
             fullCalendar.changeView(CalendarViewImpl.TIME_GRID_DAY);
         } else if (kalenderAuswahlComboBox.getValue().istWoche()) { // Beim Ändern auf die Wochenansicht passiert es, dass
             // bei Tag-Ansicht die Valid Range zu klein für eine ganze Woche ist, also kann sie auch einfach auf zwei Wochen gesetzt werden
+
             fullCalendar.setValidRange(getStartOfWeek(currentDate).minusWeeks(1), getStartOfWeek(currentDate).plusWeeks(1));
+
+            // Zunächst wird jedes Datum davor aus der anzeige entfernt
+            // Dann wird die KW berechnet
+            anzeige.removeAll();
+            anzeige.add("KW "+ getKW(currentDate.getDayOfYear()));
+
             fullCalendar.changeView(CalendarViewImpl.TIME_GRID_WEEK);
         }
         fullCalendar.setSizeFull();
@@ -373,5 +425,17 @@ public class KalenderView extends VerticalLayout {
         } else {
             return getStartOfMonth(localDate.minusDays(1));
         }
+    }
+
+    /**
+     * Gibt die Kalenderwoche (KW) aus der aktuellen Anzahl der Tage eines Jahres zurück
+     *
+     * @param tage die Tage, von dem aus die KW ermittelt werden soll
+     * @return die KW
+     * @author Leon
+     */
+    private int getKW(int tage) { //
+        return (tage / 7) + 1;  // Wir teilen durch 7, damit haben wir die KW ermittelt
+        // Zudem muss eine 1 noch aufgrund des Scopes hinzugefügt werden
     }
 }
