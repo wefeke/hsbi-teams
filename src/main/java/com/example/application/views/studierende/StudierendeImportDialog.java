9945daddbd4221a98424f0969576391019a23932
tobias <tobiasblank29@gmail.com>
2024-06-28 10:20:15 +0200

    StudierendeImportDialog update

--- src/main/java/com/example/application/views/studierende/StudierendeImportDialog.java
+++ src/main/java/com/example/application/views/studierende/StudierendeImportDialog.java
@@ -7,6 +7,7 @@ import com.example.application.security.AuthenticatedUser;
 import com.example.application.services.TeilnehmerService;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
@@ -15,7 +16,6 @@ import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import org.vaadin.lineawesome.LineAwesomeIcon;
 
-import java.io.InputStream;
 import java.util.*;
 
 public class StudierendeImportDialog extends Dialog {
@@ -25,7 +25,6 @@ public class StudierendeImportDialog extends Dialog {
     Button closeButton = new Button("Schließen");
 
     MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
-    private final Upload upload = new Upload(buffer);
     ExcelImporter excelImporter;
     Set<Teilnehmer> newTeilnehmerListe = new HashSet<>();
 
@@ -41,9 +40,10 @@ public class StudierendeImportDialog extends Dialog {
      *
      * @param teilnehmerService Der Service, der für die Verwaltung der Studierenden benötigt wird.
      * @param authenticatedUser Der aktuell authentifizierte Benutzer.
-     * @param studierendeView Die StudierendeView, die aktualisiert wird, wenn neue Teilnehmer hinzugefügt werden.
+     * @param studierendeView   Die StudierendeView, die aktualisiert wird, wenn neue Teilnehmer hinzugefügt werden.
+     * @author Tobias
      */
-    public StudierendeImportDialog(TeilnehmerService teilnehmerService, AuthenticatedUser authenticatedUser,StudierendeView studierendeView) {
+    public StudierendeImportDialog(TeilnehmerService teilnehmerService, AuthenticatedUser authenticatedUser, StudierendeView studierendeView) {
         this.excelImporter = new ExcelImporter(teilnehmerService, authenticatedUser);
 
         H2 headerTitle = new H2("Studierende Importieren");
@@ -53,51 +53,31 @@ public class StudierendeImportDialog extends Dialog {
             Dialog dialog = new Dialog();
             dialog.setMaxHeight(getHeight());
             dialog.setHeaderTitle("Neue Teilnehmer");
-            dialog.getFooter().add(new Button("OK", e ->{
+            dialog.getFooter().add(new Button("OK", e -> {
                 studierendeView.updateStudierendeView();
                 dialog.close();
             }));
-                VerticalLayout dialogLayout = new VerticalLayout();
-                dialog.add(dialogLayout);
+            VerticalLayout dialogLayout = new VerticalLayout();
+            dialog.add(dialogLayout);
 
-                for (Teilnehmer teilnehmer : newTeilnehmerListe) {
-                    Optional<User> maybeUser = authenticatedUser.get();
-                    if (maybeUser.isPresent()) {
-                        User user = maybeUser.get();
-                        teilnehmerService.saveTeilnehmer(teilnehmer, user);
-                        dialogLayout.add(new Span("Teilnehmer :" + teilnehmer.toString() + " angelegt"));
+            for (Teilnehmer teilnehmer : newTeilnehmerListe) {
+                Optional<User> maybeUser = authenticatedUser.get();
+                if (maybeUser.isPresent()) {
+                    User user = maybeUser.get();
+                    teilnehmerService.saveTeilnehmer(teilnehmer, user);
+                    dialogLayout.add(new Span("Teilnehmer :" + teilnehmer.toString() + " angelegt"));
 
-                    }
                 }
-                if (!newTeilnehmerListe.isEmpty())
-                    dialog.open();
+            }
+            if (!newTeilnehmerListe.isEmpty())
+                dialog.open();
 
             close();
 
         });
 
-        closeButton.addClickListener(event -> {
-            this.close();
-         });
-
-    upload.addSucceededListener(event -> {
-        try {
-            InputStream inputStream = buffer.getInputStream(event.getFileName());
-            newTeilnehmerListe.addAll(excelImporter.readNewTeilnehmerFromExcel(inputStream));
-
-                List<Teilnehmer> combinedItems= new ArrayList<>();
-                Optional<User> maybeUser = authenticatedUser.get();
-                if (maybeUser.isPresent()) {
-                    User user = maybeUser.get();
-                    combinedItems.addAll(teilnehmerService.findAllTeilnehmerByUserAndFilter(user, ""));
-                    combinedItems.addAll(newTeilnehmerListe);
-                }
-
-            } catch (Exception e) {
-                Notification.show("Error reading Excel file: " + e.getMessage());
-                System.out.println(e.getMessage());
-            }
-        });
+        closeButton.addClickListener(event -> this.close());
+        Upload upload = new Upload(buffer);
         upload.setUploadButton(new Button(LineAwesomeIcon.UPLOAD_SOLID.create()));
         upload.setDropLabelIcon(LineAwesomeIcon.ID_CARD.create());
         upload.setDropLabel(new Span("Teilnehmer Excel-Datei"));
@@ -108,5 +88,43 @@ public class StudierendeImportDialog extends Dialog {
         add(layout);
 
         this.getFooter().add(importButton, closeButton);
+
+        upload.addSucceededListener(event -> {
+            try {
+
+                newTeilnehmerListe.addAll(excelImporter.readNewTeilnehmerFromExcel(buffer.getInputStream(event.getFileName())));
+
+                List<Teilnehmer> combinedItems = new ArrayList<>();
+                Optional<User> maybeUser = authenticatedUser.get();
+                if (maybeUser.isPresent()) {
+                    User user = maybeUser.get();
+
+                    combinedItems.addAll(teilnehmerService.findAllTeilnehmerByUserAndFilter(user, ""));
+                    combinedItems.addAll(newTeilnehmerListe);
+
+                    Dialog dialog = new Dialog();
+                    dialog.setHeight(getHeight());
+                    dialog.setHeaderTitle(newTeilnehmerListe.size() + " neue Teilnehmer gefunden");
+                    dialog.getFooter().add(new Button("OK", e -> dialog.close()));
+
+                    if (!newTeilnehmerListe.isEmpty()) {
+                        Grid<Teilnehmer> grid = new Grid<>();
+                        grid.setItems(newTeilnehmerListe);
+                        grid.addColumn(Teilnehmer::getId).setHeader("MatrikelNr").setSortable(true).setAutoWidth(true);
+                        grid.addColumn(Teilnehmer::getVorname).setHeader("Vorname").setSortable(true).setAutoWidth(true);
+                        grid.addColumn(Teilnehmer::getNachname).setHeader("Nachname").setSortable(true).setAutoWidth(true);
+
+                        dialog.setWidth(grid.getWidth());
+                        dialog.add(grid);
+                        dialog.open();
+                    }
+
+                    System.out.println("List at end: " + newTeilnehmerListe.toString());
+                }
+            } catch (Exception e) {
+                Notification.show("Error reading Excel file: " + e.getMessage());
+                System.out.println(e.getMessage());
+            }
+        });
     }
 }
