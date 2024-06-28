784fecebb6d7ec5c99f4714cf8560a112b680c2e
tobias <tobiasblank29@gmail.com>
2024-06-28 10:52:11 +0200

    TeilnehmerHinzufuegenDialog update

--- src/main/java/com/example/application/views/veranstaltungstermin/TeilnehmerHinzufuegenDialog.java
+++ src/main/java/com/example/application/views/veranstaltungstermin/TeilnehmerHinzufuegenDialog.java
@@ -12,6 +12,8 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -28,13 +30,30 @@ public class TeilnehmerHinzufuegenDialog extends Dialog {
     private final TeilnehmerService teilnehmerService;
     private final Long veranstaltungId;
     private final Button hinzufuegenButton = new Button("Hinzufügen");
-    private final Button anlegenButton = new Button("Teilnehmer anlegen");
+    private final Button anlegenButton = new Button(new Icon(VaadinIcon.PLUS));
     private final Button importButton = new Button("Importieren");
     private final TextField filterText = new TextField();
     private final Grid<Teilnehmer> grid = new Grid<>();
     private final TeilnehmerErstellenDialog dialog;
     private final AuthenticatedUser authenticatedUser;
 
+    /**
+     * Konstruktor für den Dialog zum Hinzufügen von Teilnehmern.
+     * Initialisiert den Dialog und konfiguriert die Interaktionen der Benutzeroberfläche.
+     * Der Dialog enthält einen Grid zur Anzeige der Teilnehmer, die hinzugefügt werden können, und Buttons zum Hinzufügen, Importieren und Abbrechen.
+     * Der Hinzufügen-Button fügt die ausgewählten Teilnehmer zur Veranstaltung hinzu und aktualisiert die Ansicht.
+     * Der Import-Button öffnet einen Dialog zum Importieren von Teilnehmern.
+     * Der Abbrechen-Button schließt den Dialog.
+     *
+     * @param veranstaltungService der Service zur Verwaltung von Veranstaltungen
+     * @param teilnehmerService der Service zur Verwaltung von Teilnehmern
+     * @param veranstaltungId die ID der Veranstaltung, zu der Teilnehmer hinzugefügt werden sollen
+     * @param authenticatedUser der authentifizierte Benutzer
+     * @param veranstaltungsterminView die Ansicht des Veranstaltungstermins
+     * @param veranstaltungstermin der Veranstaltungstermin
+     * @param gruppenarbeit die Gruppenarbeit
+     * @author Tobias
+     */
     public TeilnehmerHinzufuegenDialog(VeranstaltungenService veranstaltungService, TeilnehmerService teilnehmerService, Long veranstaltungId, AuthenticatedUser authenticatedUser, VeranstaltungsterminView veranstaltungsterminView, Veranstaltungstermin veranstaltungstermin, Gruppenarbeit gruppenarbeit) {
         this.teilnehmerService = teilnehmerService;
         this.veranstaltungId = veranstaltungId;
@@ -44,12 +63,12 @@ public class TeilnehmerHinzufuegenDialog extends Dialog {
         this.setHeight("80vh");
 
         hinzufuegenButton.setEnabled(false);
-        hinzufuegenButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
-        hinzufuegenButton.getStyle().set("margin-inline-start", "auto");
+        hinzufuegenButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
 
         dialog = new TeilnehmerErstellenDialog(teilnehmerService, authenticatedUser,this);
         anlegenButton.addClickListener(event ->
-            dialog.open());
+                dialog.open());
 
         hinzufuegenButton.addClickListener(event -> {
             Set<Teilnehmer> selectedTeilnehmer = new HashSet<>(grid.getSelectedItems());
@@ -77,35 +96,57 @@ public class TeilnehmerHinzufuegenDialog extends Dialog {
                 Notification.show("Keine Teilnehmer ausgewählt", 3000, Notification.Position.MIDDLE);
             }
         });
-
+        importButton.addClickListener(event -> {
+            TeilnehmerImportDialog teilnehmerImportDialog = new TeilnehmerImportDialog(teilnehmerService, authenticatedUser, veranstaltungService, veranstaltungId, veranstaltungsterminView,this);
+            teilnehmerImportDialog.open();
+            teilnehmerImportDialog.addDialogCloseActionListener(e -> close());
+            this.close();
+        });
 
         configureGrid();
 
         Button cancelButton = new Button("Abbrechen", e -> close());
         this.setHeaderTitle("Teilnehmer hinzufügen");
+        this.getHeader().add(getToolbar());
         getFooter().add(cancelButton);
+        getFooter().add(hinzufuegenButton);
         add(
-                getToolbar(),
                 getContent()
         );
 
         updateGrid();
-
     }
 
+    /**
+     * Erstellt und gibt eine Toolbar-Komponente zurück.
+     * Die Toolbar enthält ein Textfeld zum Filtern, einen Button zum Hinzufügen und einen Button zum Importieren.
+     * Das Filter-Textfeld hat einen Platzhaltertext, einen Löschen-Button und einen Listener, der bei Änderungen die Grid-Daten aktualisiert.
+     * Die Buttons zum Hinzufügen und Importieren werden zur Toolbar hinzugefügt.
+     *
+     * @return die erstellte Toolbar als Component
+     * @author Tobias
+     */
     private Component getToolbar() {
         filterText.setPlaceholder("Suche...");
         filterText.setClearButtonVisible(true);
         filterText.setValueChangeMode(ValueChangeMode.LAZY);
         filterText.addValueChangeListener(e -> updateGrid());
 
-        HorizontalLayout toolbar = new HorizontalLayout(filterText,anlegenButton,importButton, hinzufuegenButton);
+        HorizontalLayout toolbar = new HorizontalLayout(filterText,anlegenButton,importButton);
 
         toolbar.addClassName("toolbar");
 
         return toolbar;
     }
 
+    /**
+     * Aktualisiert die Daten im Grid basierend auf dem aktuellen Suchtext.
+     * Ruft alle Teilnehmer ab, die nicht in der Veranstaltung sind, und filtert sie basierend auf dem Suchtext.
+     * Der Suchtext wird mit dem Vornamen, Nachnamen und der Matrikelnummer der Teilnehmer verglichen.
+     * Die gefilterten Teilnehmer werden dann im Grid angezeigt.
+     *
+     * @author Tobias
+     */
     public void updateGrid() {
         Optional<User> maybeUser = authenticatedUser.get();
         if (maybeUser.isPresent()) {
@@ -115,12 +156,22 @@ public class TeilnehmerHinzufuegenDialog extends Dialog {
             List<Teilnehmer> filteredTeilnehmerList = teilnehmerList.stream()
                     .filter(teilnehmer -> teilnehmer.getVorname().toLowerCase().contains(searchText)
                             || teilnehmer.getNachname().toLowerCase().contains(searchText)
-                            || Long.toString(teilnehmer.getId()).contains(searchText)) // Vergleicht den Suchtext mit der Matrikelnummer
+                            || Long.toString(teilnehmer.getId()).contains(searchText))
                     .collect(Collectors.toList());
             grid.setItems(filteredTeilnehmerList);
         }
     }
 
+    /**
+     * Konfiguriert das Grid für die Anzeige der Teilnehmer.
+     * Das Grid wird auf volle Größe gesetzt und der Auswahlmodus auf Mehrfachauswahl.
+     * Es werden Spalten für den Vornamen, Nachnamen und die Matrikelnummer der Teilnehmer hinzugefügt, die alle sortierbar sind.
+     * Ein Auswahl-Listener wird hinzugefügt, der den Hinzufügen-Button aktiviert, wenn mindestens ein Teilnehmer ausgewählt ist.
+     * Das Grid wird so eingestellt, dass mehrere Sortierungen gleichzeitig möglich sind.
+     * Schließlich wird das Grid mit den aktuellen Daten aktualisiert.
+     *
+     * @author Tobias
+     */
     private void configureGrid() {
         grid.setSizeFull();
         grid.setSelectionMode(Grid.SelectionMode.MULTI);
@@ -133,8 +184,17 @@ public class TeilnehmerHinzufuegenDialog extends Dialog {
         });
         grid.setMultiSort(true, Grid.MultiSortPriority.APPEND);
         updateGrid();
-        }
+    }
 
+    /**
+     * Erstellt und gibt eine Content-Komponente zurück.
+     * Die Content-Komponente ist ein HorizontalLayout, das das Grid enthält.
+     * Das Grid wird auf volle Größe gesetzt und erhält den FlexGrow-Wert 1, was bedeutet, dass es den verfügbaren Platz im Layout ausfüllt.
+     * Der Content erhält die CSS-Klasse "content" und wird auf volle Größe gesetzt.
+     *
+     * @return die erstellte Content-Komponente
+     * @author Tobias
+     */
     private Component getContent() {
         HorizontalLayout content = new HorizontalLayout(grid);
         content.setFlexGrow(1, grid);
@@ -142,5 +202,4 @@ public class TeilnehmerHinzufuegenDialog extends Dialog {
         content.setSizeFull();
         return content;
     }
-
 }
